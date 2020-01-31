297579a7e9c3bd2a6a91c676f9bdabdf7334b79f
inefficientRodent <60514699+inefficientRodent@users.noreply.github.com>
2020-01-31 19:39:56 +0000

    V 1.2
    Added save + delete save, will fix locations at a later date
    Some minor issues worked out

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var gameData = {
 	minorSpirit: 0,
 	mSpiritPerClick: 1,
 	vacuumUpgradeCost: 10,
+	vacuumLevel: 0,
 	pricePerMinorSpirit: 0.5,
 	
 	apparition: 0,
@@ -26,6 +27,8 @@ var gameData = {
 
 function smallSpirit() {
 	gameData.minorSpirit = gameData.minorSpirit + gameData.mSpiritPerClick
+	//All time minor spirits
+	gameData.ATminorspirit = gameData.ATminorspirit + gameData.mSpiritPerClick
 	document.getElementById("totalMinor").innerHTML = "Total wailing minor spirits: " + (Math.round(gameData.minorSpirit * 100) / 100)
 }
 
@@ -58,9 +61,14 @@ function upgradeVacuum() {
 		//Calc for increasing the cost of vacuum upgrade
 		gameData.vacuumUpgradeCost = gameData.vacuumUpgradeCost * 1.25
 		
+		//Increment Vacuum level
+		gameData.vacuumLevel += 1
 		
+		//Updating elements
 		document.getElementById("totalMinor").innerHTML = "Total wailing minor spirits: " + (Math.round(gameData.minorSpirit * 100) / 100)
 		document.getElementById("minorSpiritPerClickUpgrade").innerHTML = "Use some minor spirit to upgrade your Polturgust 3000 Cost: " + (Math.round(gameData.vacuumUpgradeCost * 100) / 100) + " Minor Spirits"
+		document.getElementById("vacuumLevel").innerHTML = "Current level: " + gameData.vacuumLevel
+		
 	}
 }
 
@@ -74,26 +82,73 @@ function upgradeVacuum() {
 function firstUpgrade() {
 	if (gameData.cash >= 25) {
 		document.getElementById("shpFirst").disabled = true;
+		
 	}
 }
 
 
-
+//Technical game bullshit
 var mainGameLoop = window.setInterval(function() {
 	//IF MINOR SPIRIT IS ABOVE 10, THEN ELEMENT WILL BE SHOWN, TADAA.
-	if (gameData.minorSpirit >= 10) {
+	if (gameData.ATminorspirit >= 10) {
 		document.getElementById("sellMoreMinorSpirit").style.display = "inline-block";
 	}
-	
-	
 	
 }, 1000)
 
 var saveGameLoop = window.setInterval(function() {
-	localStorage.setItem('ghostlyGhouls', JSON.stringify(gameData))
-}, 15000)
+	localStorage.setItem('ghostlyGhoulsSave', JSON.stringify(gameData))
+}, 300000)
 
+function manualSave() {
+	localStorage.setItem('ghostlyGhoulsSave', JSON.stringify(gameData))
+}
 
+function deleteSave() {
+	localStorage.removeItem('ghostlyGhoulsSave')
+	//Renewing variables to base numbers and also the displays. There has GOT to be an easier way to do this but oh fucking well.
+	gameData.minorSpirit= 0,
+	gameData.mSpiritPerClick= 1,
+	gameData.vacuumUpgradeCost= 10,
+	gameData.vacuumLevel= 0,
+	gameData.pricePerMinorSpirit= 0.5,
+	
+	gameData.apparition= 0,
+	
+	gameData.ghost= 0,
+	
+	gameData.cash= 0,
+		
+	//FOLLOWING VARIABLES ARE FOR "ALL TIME" STAT
+	gameData.ATcash= 0,
+	gameData.ATminorspirit= 0,
+	gameData.ATapparition= 0,
+	gameData.ATghost= 0
+	
+	
+	//Display resets
+	document.getElementById("shpFirst").disabled = false;
+	document.getElementById("sellMoreMinorSpirit").style.display = "none";
+	document.getElementById("totalMinor").innerHTML = "Total wailing minor spirits: " + (Math.round(gameData.minorSpirit * 100) / 100)
+	document.getElementById("minorSpiritPerClickUpgrade").innerHTML = "Use some minor spirit to upgrade your Polturgust 3000 Cost: " + (Math.round(gameData.vacuumUpgradeCost * 100) / 100) + " Minor Spirits"
+	document.getElementById("vacuumLevel").innerHTML = "Current level: " + gameData.vacuumLevel
+}
+
+var savegame = JSON.parse(localStorage.getItem("ghostlyGhoulsSave"))
+if (savegame !== null) {
+	gameData = savegame
+	
+	//Ensuring the game correctly loads all variables into the page immediately.
+	if (gameData.ATminorspirit >= 10) {
+		document.getElementById("sellMoreMinorSpirit").style.display = "inline-block";
+	}
+	document.getElementById("shpFirst").disabled = false;
+	document.getElementById("sellMoreMinorSpirit").style.display = "none";
+	document.getElementById("totalMinor").innerHTML = "Total wailing minor spirits: " + (Math.round(gameData.minorSpirit * 100) / 100)
+	document.getElementById("minorSpiritPerClickUpgrade").innerHTML = "Use some minor spirit to upgrade your Polturgust 3000 Cost: " + (Math.round(gameData.vacuumUpgradeCost * 100) / 100) + " Minor Spirits"
+	document.getElementById("vacuumLevel").innerHTML = "Current level: " + gameData.vacuumLevel
+	document.getElementById("totalCash").innerHTML = " You've got: $" + gameData.cash
+}
 
 
 
